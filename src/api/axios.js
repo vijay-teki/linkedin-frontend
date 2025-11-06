ee9337671d34a849145415dcc05a4542e13eaed7
TEKI VIJAYARATNAM <tekivijayaratnam12345@gmail.com>
2025-11-06 20:27:38 +0530

    deployment phase

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // Adjust for deployment later
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 API.interceptors.request.use((config) => {
